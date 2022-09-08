3a47007c02fa0c7283022e301748132550b74485
jonasngs <jonasngs30@gmail.com>
2022-09-08 17:02:04 +0800

    Create express router

--- matching-service/index.js
+++ matching-service/index.js
@@ -3,15 +3,23 @@ import cors from 'cors';
 import { createServer } from 'http';
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello World from matching-service');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello World from matching-service');
+})
+
+app.use("/api/match", router).all((_, res) => {
+  res.setHeader("content-type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 });
 
 const httpServer = createServer(app)
 
-httpServer.listen(8001);
+httpServer.listen(8001, () => console.log("matching-service listening on port 8001"));
