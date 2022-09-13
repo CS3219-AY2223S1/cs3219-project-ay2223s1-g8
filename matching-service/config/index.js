d9b7f9fec38a16ee60e05a9bbd4450e0bc504987
jonasngs <jonasngs30@gmail.com>
2022-09-14 02:13:59 +0800

    Initialize server side socket

--- matching-service/config/index.js
+++ matching-service/config/index.js
@@ -1,18 +1,18 @@
 require("dotenv/config");
 
 module.exports = {
-    development: {
-        postgres: {
-            options: {
-                host: 'localhost',
-                port: 5432,
-                database: process.env.DB_LOCAL_NAME,
-                dialect: 'postgres',
-                username: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-                logging: console.log,
-            },
-        },
+  development: {
+    postgres: {
+      options: {
+        host: "localhost",
+        port: 5433,
+        database: process.env.DB_LOCAL_NAME,
+        dialect: "postgres",
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        logging: console.log,
+      },
     },
-    production: {},
+  },
+  production: {},
 };
--- matching-service/index.js
+++ matching-service/index.js
@@ -1,28 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const database = require('./database');
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const database = require("./database");
+const { Server } = require("socket.io");
 
-const config = require('./config')[process.env.NODE_ENV || 'development'];
+const config = require("./config")[process.env.NODE_ENV || "development"];
 
 config.postgres.client = database.connectToPostgres();
 console.log(config.postgres.client);
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
-app.options('*', cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // config cors so that front-end can use
+app.options("*", cors());
 
-const MatchController = require('./controller/matchController');
+const MatchController = require("./controller/matchController");
 const matchController = new MatchController(config.postgres.client);
 
-app.get('/', (req, res) => {
-    res.send('Hello World from matching-service');
+app.get("/", (req, res) => {
+  res.send("Hello World from matching-service");
 });
 
-const httpServer = http.createServer(app)
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`SocketIO connection created, socketID=${socket.id}`);
+  socket.on("match")
+});
 
 const port = process.env.PORT || 8001;
 httpServer.listen(port);
-console.log(`Matching-service listening on port ${port} in ${app.get('env')} mode.`);
+console.log(
+  `Matching-service listening on port ${port} in ${app.get("env")} mode.`
+);
