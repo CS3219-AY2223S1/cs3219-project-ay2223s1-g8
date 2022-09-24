7b2584c9ea94717168e9c59be1015368c238980d
Wen Jin <wenjin0257@gmail.com>
2022-09-24 16:56:08 +0800

    Update theme colors and logo designs

--- frontend/src/views/landing/LandingPage.js
+++ frontend/src/views/landing/LandingPage.js
@@ -2,7 +2,7 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo-white.png";
 import pairProgGif from "../../assets/pair-programming-animate.gif";
 import "./LandingPage.scss";
 
@@ -11,33 +11,33 @@ function LandingPage() {
     <>
       <Navbar bg="light">
         <Container>
-          <Navbar.Brand href="/" className="navbar-peerprep">
+          <Navbar.Brand href="/" className="navbar-peerprep text-white">
             <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />{" "}
             PeerPrep
           </Navbar.Brand>
         </Container>
       </Navbar>
-      <Container className="landing-container" fluid>
-        <img src={pairProgGif} className="img-pair-prog" width="400" />
+      <div className="landing-container">
+        <img src={pairProgGif} className="img-pair-prog" />
         <h1 className="text-peerprep display-4">
           <strong>PeerPrep</strong>
         </h1>
-        <Container className="w-75 p-3">
+        <div className="desc-container">
           <p className="text-peerprep-desc">
             PeerPrep aims to help students and job seekers boost their technical interview skills to
             land their dream job. PeerPrep can engage in real-time, collaborative programming to
             enhance their familiarly with technical interviews.
           </p>
-        </Container>
-        <Col className="button-container">
-          <Button variant="outline-primary" href="/login">
+        </div>
+        <Col className="btn-container mt-4 mb-4">
+          <Button variant="primary text-white landing-btn" href="/login">
             Login
           </Button>
-          <Button variant="outline-primary" href="/signup">
+          <Button variant="primary text-white landing-btn" href="/signup">
             Sign Up
           </Button>
         </Col>
-      </Container>
+      </div>
     </>
   );
 }
