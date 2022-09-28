c5ba309f63baf4cdf34476772024accc510ec58f
siqi <edu.foongs@gmail.com>
2022-09-28 15:47:30 +0800

    Update env variable name to contain 'REACT_APP'

--- frontend/src/utils/configs.js
+++ frontend/src/utils/configs.js
@@ -1,1 +1,1 @@
-export const USER_SVC_BASE_URL = process.env.URI_USER_SVC || "http://localhost:8000";
+export const USER_SVC_BASE_URL = process.env.REACT_APP_URI_USER_SVC || "http://localhost:8000";
