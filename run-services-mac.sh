fa5933ca32adb8fa2430e58479b58082a75c1936
Wen Jin <wenjin0257@gmail.com>
2022-10-13 17:06:48 +0800

    Add shell script for mac in root dir to start all services in local dev

---
+++ run-services-mac.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+osascript -e 'tell application "Terminal"
+  activate
+  do script "echo Setting up services..." in front window
+  
+  my makeTab()
+  do script "cd ./frontend/" in front window
+  do script "npm ci && npm run start-mac" in front window
+  
+  my makeTab()
+  do script "cd ../user-service/" in front window
+  do script "npm ci && npm run dev" in front window
+
+  my makeTab()
+  do script "cd ../matching-service/" in front window
+  do script "npm ci && npm run dev" in front window
+
+  my makeTab()
+  do script "cd ../question-service/" in front window
+  do script "npm ci && npm run dev" in front window
+
+  my makeTab()
+  do script "cd ../communication-service/" in front window
+  do script "npm ci && npm run dev" in front window
+
+end tell
+
+on makeTab()
+  tell application "System Events" to keystroke "t" using {command down}
+  delay 0.2
+end makeTab'
