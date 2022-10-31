37dac6371f29c2c177f033474e6a58ef9e2722e0
siqi <edu.foongs@gmail.com>
2022-10-31 14:12:01 +0800

    Fix bucket issues

--- deployment/terraform/backend.tf
+++ deployment/terraform/backend.tf
@@ -1,6 +1,6 @@
 # Google cloud bucket to store tfstate file
 resource "google_storage_bucket" "default" {
-  name          = "650764b83e8fdbee-bucket-tfstate"
+  name          = "peerprep-bucket-tfstate"
   force_destroy = false
   location      = var.region
   storage_class = "STANDARD"
--- deployment/terraform/versions.tf
+++ deployment/terraform/versions.tf
@@ -9,7 +9,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "gcs" {
-    bucket = "650764b83e8fdbee-bucket-tfstate"
+    bucket = "peerprep-bucket-tfstate"
     prefix = "terraform/state"
   }
 }
