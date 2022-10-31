464cf759ea8fdb67a00fa2582c0ce3ff9a200e72
siqi <edu.foongs@gmail.com>
2022-10-31 14:24:15 +0800

    Remove bucket creation code

--- deployment/terraform/backend.tf
+++ deployment/terraform/backend.tf
@@ -1,10 +1,10 @@
 # Google cloud bucket to store tfstate file
-resource "google_storage_bucket" "default" {
-  name          = "peerprep-bucket-tfstate"
-  force_destroy = false
-  location      = var.region
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
+# resource "google_storage_bucket" "default" {
+#   name          = "peerprep-bucket-tfstate"
+#   force_destroy = false
+#   location      = var.region
+#   storage_class = "STANDARD"
+#   versioning {
+#     enabled = true
+#   }
+# }
