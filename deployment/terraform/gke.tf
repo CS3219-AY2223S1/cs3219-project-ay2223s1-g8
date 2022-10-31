4273eaa0fdb420e07fe72ae681326e6fcaa29711
siqi <edu.foongs@gmail.com>
2022-10-31 17:36:17 +0800

    Bind additional roles to sql svc account

--- deployment/terraform/gke.tf
+++ deployment/terraform/gke.tf
@@ -3,9 +3,6 @@ resource "google_container_cluster" "primary" {
   name     = "peerprep-cluster"
   location = var.region
 
-  # network    = google_compute_network.vpc.name
-  # subnetwork = google_compute_subnetwork.subnet.name
-
   # Enabling Autopilot for this cluster
   enable_autopilot = true
 }
